4d05b1a0176b386458c6b0a6eb926ab764d7b52e
Clickbyte <clickbytedev@gmail.com>
2026-02-13 20:34:43 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Invitation</title>
+</head>
+<body>
+    <div>
+        <div>
+            <h1>Invitation</h1>
+            <p>You have been invited to join Valday. Please click the link below to accept the invitation:</p>
+        </div>
+        <div style="display: flex;">
+            <button>Accept Invitation</button>
+            <button>Decline Invitation</button>
+        </div>
+    </div>
+</body>
+</html>
